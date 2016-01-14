a89aa50480a1a8aceb4cf52d5b61d9de53723ea4
Lukas Bednar <lbednar@redhat.com>
2016-01-14 13:36:20 +0100

    add setup.py to allow install it from pip
    Change-Id: I25f78cebe4fed6a84d28cd14ca52c16661f5d82a

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="otopi",
+    version="1.4.0",
+    author="???",
+    author_email="???",
+    description="???",
+    license="???",
+    keywords="otopi",
+    url="nowhere",
+    packages=['otopi'],
+    package_dir={'otopi': 'src/otopi'},
+    long_description="Otopi",
+)
